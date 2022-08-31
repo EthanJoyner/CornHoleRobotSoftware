517a7dd6a88f9ef96cac49400cd124ba62416c13
Ethan Joyner <el2joyner@gmail.com>
2022-08-31 19:27:17 -0400

    Web HTML and CSS

---
+++ Audrino Code/Serial_read/Serial_read.ino
@@ -0,0 +1,39 @@
+const unsigned int MAX_MESSAGE_LENGTH = 12;
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+
+ //Check to see if anything is available in the serial receive buffer
+ while (Serial.available() > 0)
+ {
+   //Create a place to hold the incoming message
+   static char message[MAX_MESSAGE_LENGTH];
+   static unsigned int message_pos = 0;
+
+   //Read the next available byte in the serial receive buffer
+   char inByte = Serial.read();
+
+   //Message coming in (check not terminating character) and guard for over message size
+   if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
+   {
+     //Add the incoming byte to our message
+     message[message_pos] = inByte;
+     message_pos++;
+   }
+   //Full message received...
+   else
+   {
+     //Add null character to string
+     message[message_pos] = '\0';
+
+     //Print the message (or do other things)
+     Serial.println(message);
+
+     //Reset for the next message
+     message_pos = 0;
+   }
+ }
+}
